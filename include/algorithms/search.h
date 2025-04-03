b81f0a8af3e1f57d44718910e2267768b2549b4f
VerdantE1 <2523877046@qq.com>
2025-04-03 16:08:53 +0800

    - 增加二分搜索：增加值域搜所模板

--- include/algorithms/search.h
+++ include/algorithms/search.h
@@ -4,11 +4,61 @@
 
 #ifndef ALGORITHMS_SEARCH_H
 #define ALGORITHMS_SEARCH_H
-
+using namespace  std;
 #include <vector>
+#include <functional>
+
+
+//1.为vector<int>服务
+//2.
+
 namespace frank{
-    //二分搜索算法：返回目标值的索引，如果未找到返回 -1
-    int binarySearch(const std::vector<int>&nums, int target);
+    //模板 1：查找某个元素的下标（找第一个等于 target 的位置） 需要正序数值
+    int binarySearch(const std::vector<int>&nums, int target){
+        int left =0 ,right =nums.size() -1;
+        while(left <= right){
+            int mid = left + (right-left) /2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] < target)  left = mid+1;
+            else right = mid-1;
+        }
+        return left;
+    }
+
+    //模板 2：寻找第一个满足条件的位置（左边界/最小值）
+    int binaryLeft(const std::vector<int>& nums, const function<bool(int)>& check){
+        int left =0,right=nums.size();
+        while(left < right){
+            int mid = left + (right-left) /2;
+            if(check(nums[mid])) {
+                right = mid;  //找左边界，向左缩小范围
+            }
+            else left= mid+1;
+        }
+        //此时left == right
+        //由于每次check成功后都将right = mid,所以当结束后left就是答案
+        if(left < nums.size() && check(nums[left]))   return left;
+        else return -1;
+    }
+
+    //模板 3：寻找最后一个满足条件的位置（右边界/最大值）
+    int binaryRight(const std::vector<int>& nums, const function<bool(int)>& check) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (check(nums[mid])) {
+                left = mid+1;// 满足条件，尝试找更靠右的
+            } else {
+                right = mid; // 不满足，缩小范围
+            }
+        }
+        //此时left == right,是第一个不满足check的元素位置
+        //由于每次check成功后都将 left = mid+1,所以left-1是答案；
+        if(left == 0) return -1;
+        else return left -1 ;
+    }
+
+    //模板 4：二分切割问题（如两个有序数组找中位数）
 }
 
 #endif //ALGORITHMS_SEARCH_H
--- main.cpp
+++ main.cpp
@@ -15,40 +15,6 @@ const int MOD =1e9+7;
 
 int main(){
     int m ,n; cin >> m >>n;
-    FastStateEncoder encoder(m,3); //定义一个编码器，m位，3进制
-
-    auto States = encoder.getAllValidStates(makeAdjacentDifferentValidator(m,3)); //得到所有不相邻的符合状态
-    int sz = States.size();
-
-    //i行兼容j的状态
-    vector<vector<int>> compat(sz);
-    vector<vector<int>> dp(n, vector<int>(sz, 0));
-
-    // 初始化第一列
-    for (int i = 0; i < sz; i++) {
-        dp[0][i] = 1;
-    }
-    const int MOD = 1e9 + 7;
-
-    for(int col =1;col<n;col++){
-        for(int i =0;i<sz;i++){
-            int i_state = States[i];
-            for(int j =0;j<sz;j++){
-                int j_states = States[j];
-                if(encoder.isCompatible(i_state,j_states,makeNoSamePositionChecker(m,3))){
-                    dp[col][i] += dp[col-1][j];
-                    dp[col][i] %= MOD;
-                }
-            }
-        }
-    }
-    int result = 0;
-    for (int i = 0; i < sz; i++) {
-        result = (result + dp[n - 1][i]) % MOD;
-    }
-
-    cout << result <<endl;
-
 
     return 0;
 }
